e9cd49853e3e14deaa47dbe9bc3e754763e7087d
gabiolifer <gabiof121@gmail.com>
2024-05-06 20:43:23 -0300

    novas alterações

--- corrente/Program.cs
+++ corrente/Program.cs
@@ -13,10 +13,11 @@ namespace corrente
 {
     public class Program
     {
-        public List<CCorrente> contas=new List<CCorrente>();
+        static List<CCorrente> contas=new List<CCorrente>();
 
         public void Main(string[] args)
         {
+            
             string? op;
             do
             {
@@ -32,9 +33,9 @@ namespace corrente
                     case "1": MenuAcessoAdministrativo(); break;
                     case "2":
                         Console.WriteLine("digite o numero da conta: ");
-                        string? numeroConta = Console.ReadLine();
+                        string numeroConta = Console.ReadLine();
 
-                        if (contas.Contains(numeroConta))
+                        if (contas.Exists(c => c.numero == numeroConta))
                         {
                             MenuCaixaEletronico(); break;
                         }
@@ -48,83 +49,168 @@ namespace corrente
                         break;
                     default: Console.WriteLine("invalido, redigite."); break;
                 }
-
-                public void MenuAcessoAdministrativo()
-                {
-                    string? opAD;
-                    do
-                    {
-                        Console.WriteLine("Menu");
-                        Console.WriteLine("1. Cadastro conta corrente");
-                        Console.WriteLine("2. Mostrar saldo de todas as contas");
-                        Console.WriteLine("3. Excluir conta");
-                        Console.WriteLine("4. Voltar ao menu anterior");
-                        Console.Write("escolha:");
-                        opAD = Console.ReadLine();
-
-                        switch (opAD)
-                        {
-                            case "1":
-                                CadastroContaCorrente();
-                                break;
-                            case "2":
-                                MostrarSaldoContas();
-                                break;
-                            case "3":
-                                ExcluirConta();
-                                break;
-                            case "4":
-                                return;
-                            default:
-                                Console.WriteLine("Opção inválida. Tente novamente.");
-                                break;
-                        }
-                    } while (opAD != "4");
-                }
-
-                public void MenuCaixaEletronico()
-                {
-                    string? opCE;
-                    do
-                    {
-                        Console.WriteLine("Menu");
-                        Console.WriteLine("1. Saque");
-                        Console.WriteLine("2. Deposito");
-                        Console.WriteLine("3. Transferencia");
-                        Console.WriteLine("4. Voltar ao menu anterior");
-                        Console.Write("escolha:");
-                        opCE = Console.ReadLine();
-
-                        switch (opCE)
-                        {
-                            case "1":
-                                Console.WriteLine("digite o valor a sacar:");
-                                string i= Console.ReadLine();
-                                double valorS;
-                                Double.TryParse.ReadLine(i, out valorS);
-                                Sacar(valorS);
-                                break;
-                            case "2":
-                                Console.WriteLine("digite o valor a depositar:");
-                                string j = Console.ReadLine();
-                                double valorD;
-                                Double.TryParse.ReadLine(j, out valorD);
-                                Depositar(valorD);
-                                break;
-                            case "3":
-                                ExcluirConta();
-                                break;
-                            case "4":
-                                return;
-                            default:
-                                Console.WriteLine("Opção inválida. Tente novamente.");
-                                break;
-                        }
-                    } while (opAD != "4");
-                }
             } while (op != "3");
+        }
 
+        void MenuAcessoAdministrativo()
+        {
+            string? opAD;
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Cadastro conta corrente");
+                Console.WriteLine("2. Mostrar saldo de todas as contas");
+                Console.WriteLine("3. Excluir conta");
+                Console.WriteLine("4. Voltar ao menu anterior");
+                Console.Write("escolha:");
+                opAD = Console.ReadLine();
+
+                switch (opAD)
+                {
+                    case "1":
+                        CadastroContaCorrente();
+                        break;
+                    case "2":
+                        MostrarSaldoContas();
+                        break;
+                    case "3":
+                        ExcluirConta();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            } while (opAD != "4");
+        }
+
+        void CadastroContaCorrente()
+        {
+            Console.Write("Digite o número da conta: ");
+            string numero = Console.ReadLine();
+            Console.Write("Digite o limite da conta: ");
+            double limite = Convert.ToDouble(Console.ReadLine());
+
+            CCorrente novaConta = new CCorrente(numero, limite);
+            contas.Add(novaConta);
+            Console.WriteLine("Conta cadastrada com sucesso.");
+        }
+
+        void MostrarSaldoContas()
+        {
+            Console.WriteLine("----- Saldo de Todas as Contas -----");
+            for (int n = 0; n < contas.Count; n++) //pq nn Capacity??
+            {
+                Console.WriteLine($"Conta: {contas[n + 1].numero} - Saldo: {contas[n + 1].saldo}");
+            }
+        }
+
+        void ExcluirConta()
+        {
+            Console.WriteLine("digite o numero da conta a ser excluída: ");
+            string numeroConta = Console.ReadLine();
+
+            Console.WriteLine("digite o limite da conta a ser excluída: ");
+            double limiteConta = Double.Parse(Console.ReadLine());
+
+            CCorrente conta = contas.Find(c => c.numero == numeroConta && c.limite == limiteConta);
+
+            if (conta!=null)
+            {
+                conta.status = false;
+                Console.WriteLine("Conta excluída com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("a conta não existe");
+                return;
+            }
             
+        }
+
+        void MenuCaixaEletronico()
+        {
+            string? opCE;
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Saque");
+                Console.WriteLine("2. Deposito");
+                Console.WriteLine("3. Transferencia");
+                Console.WriteLine("4. Voltar ao menu anterior");
+                Console.Write("escolha:");
+                opCE = Console.ReadLine();
+
+                switch (opCE)
+                {
+                    case "1":
+                        Sacar();
+                        break;
+                    case "2":
+                        Depositar();
+                        break;
+                    case "3":
+                        Transferir();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            } while (opCE != "4");
+        }
+
+        void Sacar()
+        {
+            Console.WriteLine("digite o valor do saque:");
+            double saque = Double.Parse(Console.ReadLine());
+
+            foreach (double saldo in ??)
+            {
+                if (saldo <= 0)
+                {
+                    Console.WriteLine("Não há saldo disponível.");
+                    return;
+                }
+                else if (saque > saldo)
+                {
+                    int opSaqueSaida;
+                    Console.WriteLine($"\nO valor de saque desejado que é maior que o valor do saldo da conta. Deseja retornar ao menu anterior ou sacar todo o valor" +
+                        $"presente nesta conta, de R$ {saldo} ? Caso queira voltar, digite 1. Caso contrário, digite 2.\n");
+                    Console.WriteLine("Opção escolhida: ");
+                    opSaqueSaida = Int32.Parse(Console.ReadLine());
+                    if (opSaqueSaida == 1)
+                    {
+                        Console.WriteLine("Retornando ao menu anterior...\n");
+                        return;
+                    }
+                    else if (opSaqueSaida == 2)
+                    {
+                        saque == saldo;
+                        saldo -= saque;
+                        Console.WriteLine($"\nO saque no valor de {saque} desta conta corrente foi realizado com sucesso, tornando seu saldo equivalente a {saldo} reais. ");
+                    }
+                }
+                else
+                {
+                    saldo -= valorSaque;
+                    Console.WriteLine($"\n Saque de {valorSaque} realizado com sucesso. Saldo atual da conta : R$ {saldo}.");
+                }
+            }
+        }
+
+        void Depositar()
+        {
+            double valord;
+
+            //como o programa sabe q conta q é??
+        }
+
+        void Transferir()
+        {
+
         }
     }
 }
